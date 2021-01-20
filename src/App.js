24e922e5fe491a32563174fced68faeceed90028
Matheus <theus-7@hotmail.com>
2021-01-20 00:06:17 -0300

    improved layout and animation

--- src/App.js
+++ src/App.js
@@ -8,88 +8,98 @@ import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import InfoIcon from '@material-ui/icons/Info';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 function App() {
 
-  const [userData, setUserData] = useState({});
+  const [userData, setUserData] = useState(false);
 
   useEffect(() => {
     axios.get('https://api.github.com/users/theusf').then(response => {
-      setUserData(response.data);
-      console.log(userData)
+
+      setTimeout(() => { setUserData(response.data); }, 50)
+
     })
 
-  }, []);
+  });
 
 
-  return (
+  return !userData ?
+
     <div className="app">
+      <CircularProgress size={200}/>
+    </div> :
+
+    (
+      <div className="app">
 
 
-      <div className="profile">
+        <div className="profile">
 
-        <div className="profile__coluna1">
-          <div className="profile__info">
-            <div className="profile_image_and_user">
-              <Avatar src={userData.avatar_url} className="profile__image" />
-              <h1>
-                {userData.name}
-              </h1>
-              <p>@{userData.login}</p>
-              <p>{userData.company}</p>
-              <p>{userData.location}</p> 
+          <div className="profile__coluna1">
+            <div className="profile__info">
+              <div className="profile_image_and_user">
+                <Avatar src={userData.avatar_url} style={{ height: '200px', width: '200px' }} />
+                <h1>
+                  {userData.name}
+                </h1>
+                <p>@{userData.login}</p>
 
-              <div className="profile__button">
-                <Button
-                  variant="outlined"
-                  color={'inherit'}
-                  startIcon={<GitHubIcon />}
-                  href={userData.html_url}
-                >
-                  Abrir perfil
+                <br />
+                <p>{userData.company}</p>
+                <p>{userData.location}</p>
+
+                <div className="profile__button">
+                  <Button
+                    variant="outlined"
+                    color={'inherit'}
+                    startIcon={<GitHubIcon />}
+                    href={userData.html_url}
+                  >
+                    Abrir perfil
               </Button>
-              </div>
+                </div>
 
 
-            </div >
+              </div >
+            </div>
+
+
+            <div className="profile__info">
+              <h1>  <InfoIcon /> Bio </h1>
+              <p> {userData.bio}</p>
+
+            </div>
+
           </div>
 
+          <div className="profile__coluna2">
+            <div className="profile__info">
+              <h1>  Repositórios </h1>
+              <h2>
+                <BookIcon /> <CountUp start={0} end={userData.public_repos ? userData.public_repos : 10} duration={5} delay={1}/>
+              </h2>
+            </div>
 
-          <div className="profile__info">
-            <h2>  <InfoIcon /> Bio </h2>
-            <p> {userData.bio}</p>
+            <div className="profile__info">
+              <h1>Seguidores </h1>
+              <h2>
+                <SupervisedUserCircleIcon /> <CountUp start={0} end={userData.followers ? userData.followers : 1} duration={5} delay={1}/>
+              </h2>
+            </div>
 
+            <div className="profile__info">
+              <h1>  Seguindo </h1>
+              <h2>
+                <FavoriteIcon /> <CountUp start={0} end={userData.following ? userData.following : 1} duration={5} delay={1}/>
+              </h2>
+            </div>
           </div>
 
-        </div>
-
-        <div className="profile__coluna2">
-          <div className="profile__info">
-            <h3>  Repositórios </h3>
-            <h1>
-              <BookIcon /> <CountUp start={0} end={userData.public_repos ? userData.public_repos : 10} duration={5} />
-            </h1>
-          </div>
-
-          <div className="profile__info">
-            <h3>Seguidores </h3>
-            <h1>
-              <SupervisedUserCircleIcon /> <CountUp start={0} end={userData.followers ? userData.followers : 1} duration={5} />
-            </h1>
-          </div>
-
-          <div className="profile__info">
-            <h3>  Seguindo </h3>
-            <h1>
-              <FavoriteIcon /> <CountUp start={0} end={userData.following ? userData.following : 1} duration={5} />
-            </h1>
-          </div>
         </div>
 
       </div>
-
-    </div>
-  );
+    );
 }
 
 export default App;
